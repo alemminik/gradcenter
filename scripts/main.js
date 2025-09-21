e9795c28adf8671621d0321b3b2f1a5c1b67846e
Игорь Денисенков <igordenisenkov@MacBook-Air-Igor.local>
2025-09-21 13:50:46 +0300

    fix cases adaptive

--- scripts/main.js
+++ scripts/main.js
@@ -286,7 +286,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         loop: false,
         slidesPerView: 1,
-        spaceBetween: 0,
+        spaceBetween: 15,
         grabCursor: true,
         ceneteredSlides: true,
         breakpoints: {
